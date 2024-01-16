36c695831e2d79da001a9c18de846ee2e2a97368
ramanaguru <ramanaguruloyola@gmail.com>
2024-01-16 23:45:36 +0530

    Added mysql driver for Db Connectivity and create Db in mysql workbench

--- src/main/java/dev/ramana/productservice/models/BaseModel.java
+++ src/main/java/dev/ramana/productservice/models/BaseModel.java
@@ -1,10 +1,29 @@
 package dev.ramana.productservice.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
-    private Long id;
+    @Id
+    @GeneratedValue(generator =  "uuidgenrator")
+    @GenericGenerator(name = "uuidgenerator" , strategy = "uuid2")
+    @Column(name = "id" , columnDefinition = "binary(16)" , nullable = false, updatable = false)
+    private UUID uuid;
 }
+
+/*Inherientence involves in our system what to do?
+
+        Oneway ==> use @MappedSuperclass
+                what is @MappedSuperClass==> create table for children but not for parent
+
+
+        2way) ==> Single Table
+
+ */
--- src/main/java/dev/ramana/productservice/models/Product.java
+++ src/main/java/dev/ramana/productservice/models/Product.java
@@ -1,14 +1,20 @@
 package dev.ramana.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private double price;
+
+    @ManyToOne //One product have many category and one category have one  product  ==> M : 1
     private Category category;
+
     private String image;
 }
